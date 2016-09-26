695e506ef74bac9f5746feaf874ebeab9dfc9867
littlecommie <siryk.anna@gmail.com>
2016-09-26 12:56:34 -0400

    Final (?) adjustments to train controller UI.
    Deleted simulation speed controller, added service brake.

--- src/trainController/trainControllerUI.java
+++ src/trainController/trainControllerUI.java
@@ -32,6 +32,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 import javax.swing.Box;
 import javax.swing.JSlider;
+import java.awt.SystemColor;
 
 public class trainControllerUI extends JFrame {
 
@@ -70,7 +71,7 @@ public class trainControllerUI extends JFrame {
 		setTitle("Train Controller");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(trainControllerUI.class.getResource("/TrainController/computer1.jpg")));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 695, 580);
+		setBounds(100, 100, 695, 583);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -97,55 +98,59 @@ public class trainControllerUI extends JFrame {
 		SpeedLimit = new JTextField();
 		SpeedLimit.setHorizontalAlignment(SwingConstants.CENTER);
 		SpeedLimit.setEditable(false);
-		SpeedLimit.setBounds(518, 146, 86, 28);
+		SpeedLimit.setBounds(518, 103, 86, 28);
 		contentPane.add(SpeedLimit);
 		SpeedLimit.setColumns(10);
 		SpeedLimit.setText("40 mph"); //FOR SHOWING OFF PURPOSES ONLY, DELETE LATER
 		
 		JLabel lblSpeedLimit = new JLabel("SPEED LIMIT");
 		lblSpeedLimit.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSpeedLimit.setBounds(518, 129, 86, 14);
+		lblSpeedLimit.setBounds(518, 86, 86, 14);
 		contentPane.add(lblSpeedLimit);
 		
 		SpeedCurr = new JTextField();
 		SpeedCurr.setHorizontalAlignment(SwingConstants.CENTER);
 		SpeedCurr.setEditable(false);
-		SpeedCurr.setBounds(518, 194, 86, 28);
+		SpeedCurr.setBounds(518, 151, 86, 28);
 		contentPane.add(SpeedCurr);
 		SpeedCurr.setColumns(10);
 		SpeedCurr.setText("35 mph"); //FOR SHOWING OFF PURPOSES ONLY, DELETE LATER
 		
 		JLabel lblCurrentSpeed = new JLabel("Current Speed");
 		lblCurrentSpeed.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentSpeed.setBounds(518, 177, 86, 14);
+		lblCurrentSpeed.setBounds(518, 134, 86, 14);
 		contentPane.add(lblCurrentSpeed);
 		
 		SpeedReq = new JTextField();
 		SpeedReq.setHorizontalAlignment(SwingConstants.CENTER);
-		SpeedReq.setBounds(518, 249, 86, 28);
+		SpeedReq.setBounds(518, 206, 86, 28);
 		contentPane.add(SpeedReq);
 		SpeedReq.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Request Speed");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(518, 232, 86, 14);
+		lblNewLabel.setBounds(518, 189, 86, 14);
 		contentPane.add(lblNewLabel);
 		
 		JButton btnSpeedReq = new JButton("Go");
-		btnSpeedReq.setBounds(614, 249, 55, 28);
+		btnSpeedReq.setBounds(614, 206, 55, 28);
 		contentPane.add(btnSpeedReq);
 		
 		JButton btnEmergencyBrake = new JButton("Emergency Brake");
+		btnEmergencyBrake.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
 		btnEmergencyBrake.setBackground(UIManager.getColor("Button.background"));
 		btnEmergencyBrake.setForeground(Color.RED);
-		btnEmergencyBrake.setBounds(497, 280, 134, 23);
+		btnEmergencyBrake.setBounds(497, 268, 134, 23);
 		contentPane.add(btnEmergencyBrake);
 		
 		JButton imgLight = new JButton("");
 		imgLight.setBackground(UIManager.getColor("Button.disabledShadow"));
 		imgLight.setEnabled(false);
 		imgLight.setIcon(new ImageIcon(trainControllerUI.class.getResource("/TrainController/LightOnTiny.png")));
-		imgLight.setBounds(143, 129, 43, 46);
+		imgLight.setBounds(143, 86, 43, 46);
 		contentPane.add(imgLight);
 		
 		JToggleButton tglbtnLights = new JToggleButton("Lights");
@@ -158,14 +163,14 @@ public class trainControllerUI extends JFrame {
 				}
 			}
 		});
-		tglbtnLights.setBounds(10, 129, 107, 46);
+		tglbtnLights.setBounds(10, 86, 107, 46);
 		contentPane.add(tglbtnLights);
 		
 		txtRightDoors = new JTextField();
 		txtRightDoors.setHorizontalAlignment(SwingConstants.CENTER);
 		txtRightDoors.setEditable(false);
 		txtRightDoors.setText("Closed");
-		txtRightDoors.setBounds(127, 200, 75, 28);
+		txtRightDoors.setBounds(127, 157, 75, 28);
 		contentPane.add(txtRightDoors);
 		txtRightDoors.setColumns(10);
 		
@@ -179,7 +184,7 @@ public class trainControllerUI extends JFrame {
 				}
 			}
 		});
-		tglbtnRightDoors.setBounds(10, 200, 107, 28);
+		tglbtnRightDoors.setBounds(10, 157, 107, 28);
 		contentPane.add(tglbtnRightDoors);
 		
 		txtLeftDoors = new JTextField();
@@ -187,7 +192,7 @@ public class trainControllerUI extends JFrame {
 		txtLeftDoors.setHorizontalAlignment(SwingConstants.CENTER);
 		txtLeftDoors.setEditable(false);
 		txtLeftDoors.setColumns(10);
-		txtLeftDoors.setBounds(127, 232, 75, 28);
+		txtLeftDoors.setBounds(127, 189, 75, 28);
 		contentPane.add(txtLeftDoors);
 		
 		JToggleButton tglbtnLeftDoors = new JToggleButton("Left Doors");
@@ -201,65 +206,65 @@ public class trainControllerUI extends JFrame {
 				}
 			}
 		});
-		tglbtnLeftDoors.setBounds(10, 232, 107, 28);
+		tglbtnLeftDoors.setBounds(10, 189, 107, 28);
 		contentPane.add(tglbtnLeftDoors);
 		
 		TempCurr = new JTextField();
 		TempCurr.setHorizontalAlignment(SwingConstants.CENTER);
 		TempCurr.setEditable(false);
 		TempCurr.setColumns(10);
-		TempCurr.setBounds(10, 294, 86, 28);
+		TempCurr.setBounds(10, 251, 86, 28);
 		contentPane.add(TempCurr);
 		TempCurr.setText("68 \u2109"); //FOR SHOWING OFF PURPOSES ONLY, DELETE LATER
 		
 		JLabel lblCurrentTemp = new JLabel("Current Temp");
 		lblCurrentTemp.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentTemp.setBounds(10, 277, 86, 14);
+		lblCurrentTemp.setBounds(10, 234, 86, 14);
 		contentPane.add(lblCurrentTemp);
 		
 		TempReq = new JTextField();
 		TempReq.setHorizontalAlignment(SwingConstants.CENTER);
 		TempReq.setColumns(10);
-		TempReq.setBounds(10, 340, 86, 28);
+		TempReq.setBounds(10, 297, 86, 28);
 		contentPane.add(TempReq);
 		
 		JLabel lblRequestTemp = new JLabel("Request Temp");
 		lblRequestTemp.setHorizontalAlignment(SwingConstants.CENTER);
-		lblRequestTemp.setBounds(10, 325, 86, 14);
+		lblRequestTemp.setBounds(10, 282, 86, 14);
 		contentPane.add(lblRequestTemp);
 		
 		JButton btnTempReq = new JButton("Go");
-		btnTempReq.setBounds(106, 339, 55, 28);
+		btnTempReq.setBounds(106, 296, 55, 28);
 		contentPane.add(btnTempReq);
 		
 		JTextArea AnnCurr = new JTextArea();
 		AnnCurr.setEditable(false);
 		AnnCurr.setLineWrap(true);
-		AnnCurr.setBounds(143, 401, 383, 33);
+		AnnCurr.setBounds(143, 404, 383, 33);
 		contentPane.add(AnnCurr);
 		
 		JLabel lblCurrentAnnouncement = new JLabel("Current Announcement");
 		lblCurrentAnnouncement.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentAnnouncement.setBounds(227, 386, 225, 14);
+		lblCurrentAnnouncement.setBounds(227, 389, 225, 14);
 		contentPane.add(lblCurrentAnnouncement);
 		
 		JLabel lblCustomAnnouncement = new JLabel("Custom Announcement");
 		lblCustomAnnouncement.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCustomAnnouncement.setBounds(227, 449, 225, 14);
+		lblCustomAnnouncement.setBounds(227, 452, 225, 14);
 		contentPane.add(lblCustomAnnouncement);
 		
 		JTextArea AnnCust = new JTextArea();
 		AnnCust.setToolTipText("");
 		AnnCust.setLineWrap(true);
-		AnnCust.setBounds(143, 464, 383, 33);
+		AnnCust.setBounds(143, 467, 383, 33);
 		contentPane.add(AnnCust);
 		
 		JButton btnAnnCustReq = new JButton("Go");
-		btnAnnCustReq.setBounds(550, 464, 64, 33);
+		btnAnnCustReq.setBounds(550, 467, 64, 33);
 		contentPane.add(btnAnnCustReq);
 		
 		JButton btnAnnNextReq = new JButton("Make \"Next Stop\" Announcement");
-		btnAnnNextReq.setBounds(227, 506, 225, 23);
+		btnAnnNextReq.setBounds(227, 509, 225, 23);
 		contentPane.add(btnAnnNextReq);
 		
 		JTextPane txtMessages = new JTextPane();
@@ -281,23 +286,29 @@ public class trainControllerUI extends JFrame {
 		});
 		//*******************************************************************************************
 		txtMessages.setEditable(false);
-		txtMessages.setBounds(227, 129, 225, 230);
+		txtMessages.setBounds(227, 86, 225, 274);
 		contentPane.add(txtMessages);
 		
 		JLabel lblSystemMessages = new JLabel("System Messages");
 		lblSystemMessages.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSystemMessages.setBounds(227, 104, 225, 14);
+		lblSystemMessages.setBounds(227, 61, 225, 14);
 		contentPane.add(lblSystemMessages);
 		
 		JToggleButton tglbtnAutomatic = new JToggleButton("Automatic");
 		tglbtnAutomatic.setSelected(true);
 		
-		tglbtnAutomatic.setBounds(469, 11, 101, 20);
+		tglbtnAutomatic.setBounds(469, 30, 101, 20);
 		contentPane.add(tglbtnAutomatic);
 		
 		JToggleButton tglbtnManual = new JToggleButton("Manual");
-		tglbtnManual.setBounds(568, 11, 101, 20);
+		tglbtnManual.setBounds(568, 30, 101, 20);
 		contentPane.add(tglbtnManual);
+		
+		JButton btnServiceBrake = new JButton("Service Brake");
+		btnServiceBrake.setForeground(Color.BLACK);
+		btnServiceBrake.setBackground(SystemColor.menu);
+		btnServiceBrake.setBounds(497, 245, 134, 23);
+		contentPane.add(btnServiceBrake);
 		
 		tglbtnAutomatic.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -310,6 +321,7 @@ public class trainControllerUI extends JFrame {
 					btnAnnCustReq.setEnabled(false);
 					btnAnnNextReq.setEnabled(false);
 					btnSpeedReq.setEnabled(false);
+					btnServiceBrake.setEnabled(false);
 				} else { //selects Manual button, enables Manual buttons
 					tglbtnManual.setSelected(true);
 					tglbtnLights.setEnabled(true);
@@ -319,6 +331,7 @@ public class trainControllerUI extends JFrame {
 					btnAnnCustReq.setEnabled(true);
 					btnAnnNextReq.setEnabled(true);
 					btnSpeedReq.setEnabled(true);
+					btnServiceBrake.setEnabled(true);
 				}
 			}
 		});
@@ -333,6 +346,7 @@ public class trainControllerUI extends JFrame {
 					btnAnnCustReq.setEnabled(true);
 					btnAnnNextReq.setEnabled(true);
 					btnSpeedReq.setEnabled(true);
+					btnServiceBrake.setEnabled(true);
 				} else { //selects Automatic button, disables Manual buttons
 					tglbtnAutomatic.setSelected(true);
 					tglbtnLights.setEnabled(false);
@@ -342,9 +356,33 @@ public class trainControllerUI extends JFrame {
 					btnAnnCustReq.setEnabled(false);
 					btnAnnNextReq.setEnabled(false);
 					btnSpeedReq.setEnabled(false);
+					btnServiceBrake.setEnabled(false);
 				}
 			}
 		});
+		
+		JLabel lblCurrentPower = new JLabel("Current Power");
+		lblCurrentPower.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCurrentPower.setBounds(518, 313, 86, 14);
+		contentPane.add(lblCurrentPower);
+		
+		PowerCurr = new JTextField();
+		PowerCurr.setHorizontalAlignment(SwingConstants.CENTER);
+		PowerCurr.setEditable(false);
+		PowerCurr.setColumns(10);
+		PowerCurr.setBounds(518, 330, 86, 28);
+		contentPane.add(PowerCurr);
+		PowerCurr.setText("10 W"); //FOR SHOWING OFF PURPOSES ONLY, DELETE LATER (disclaimer: I know nothing about power)
+		
+		JLabel TTEIcon = new JLabel("");
+		TTEIcon.setIcon(new ImageIcon(trainControllerUI.class.getResource("/shared/TTESmall.png")));
+		TTEIcon.setBounds(10, 468, 64, 65);
+		contentPane.add(TTEIcon);
+		
+		JLabel lblTrainId = new JLabel("Train ID");
+		lblTrainId.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTrainId.setBounds(10, 11, 162, 14);
+		contentPane.add(lblTrainId);
 		
 		//Initialize for default Automatic mode ************
 		tglbtnLights.setEnabled(false);
@@ -354,50 +392,7 @@ public class trainControllerUI extends JFrame {
 		btnAnnCustReq.setEnabled(false);
 		btnAnnNextReq.setEnabled(false);
 		btnSpeedReq.setEnabled(false);
+		btnServiceBrake.setEnabled(false);
 		//**************************************************
-		
-		JLabel lblCurrentPower = new JLabel("Current Power");
-		lblCurrentPower.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCurrentPower.setBounds(518, 325, 86, 14);
-		contentPane.add(lblCurrentPower);
-		
-		PowerCurr = new JTextField();
-		PowerCurr.setHorizontalAlignment(SwingConstants.CENTER);
-		PowerCurr.setEditable(false);
-		PowerCurr.setColumns(10);
-		PowerCurr.setBounds(518, 342, 86, 28);
-		contentPane.add(PowerCurr);
-		PowerCurr.setText("10 W"); //FOR SHOWING OFF PURPOSES ONLY, DELETE LATER (disclaimer: I know nothing about power)
-		
-		JLabel TTEIcon = new JLabel("");
-		TTEIcon.setIcon(new ImageIcon(trainControllerUI.class.getResource("/shared/TTESmall.png")));
-		TTEIcon.setBounds(10, 465, 64, 65);
-		contentPane.add(TTEIcon);
-		
-		JSlider sliderSimSpeed = new JSlider();
-		sliderSimSpeed.setMaximum(10);
-		sliderSimSpeed.setMinimum(1);
-		sliderSimSpeed.setValue(1);
-		sliderSimSpeed.setSnapToTicks(true);
-		sliderSimSpeed.setPaintTicks(true);
-		sliderSimSpeed.setPaintLabels(true);
-		sliderSimSpeed.setMajorTickSpacing(1);
-		sliderSimSpeed.setBounds(469, 62, 200, 38);
-		contentPane.add(sliderSimSpeed);
-		
-		JLabel lblSimulationSpeed = new JLabel("Simulation Speed");
-		lblSimulationSpeed.setHorizontalAlignment(SwingConstants.CENTER);
-		lblSimulationSpeed.setBounds(469, 42, 200, 14);
-		contentPane.add(lblSimulationSpeed);
-		
-		//Only unnecessary if we find out we need separate simulation speeds on our UIs
-		sliderSimSpeed.setVisible(false);
-		lblSimulationSpeed.setVisible(false);
-		//******************************************************************************
-		
-		JLabel lblTrainId = new JLabel("Train ID");
-		lblTrainId.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTrainId.setBounds(10, 11, 162, 14);
-		contentPane.add(lblTrainId);
 	}
 }
