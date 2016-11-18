5068f3183358087e09bdbea58aaf01f4cd96a549
Tom Bertrand <tpb18@pitt.edu>
2016-11-18 11:27:08 -0500

    Added method for changing color of track buttons

--- src/ctcOffice/TrackButton.java
+++ src/ctcOffice/TrackButton.java
@@ -1,6 +1,8 @@
 package ctcOffice;
 
 import javax.swing.*;
+import java.awt.*;
+import trackModel.TrackBlock.*;
 
 /**
  * Created by Tom on 10/12/2016.
@@ -18,5 +20,19 @@ public class TrackButton extends JToggleButton
     public String toString()
     {
         return line + " Line: Block " + block;
+    }
+
+    /**
+     * Sets the color of the TrackButton to match the status of the corresponding block
+     * @param status The status of the track block
+     */
+    public void setStatus(trackStatus status) {
+        if (status == trackStatus.UNOCCUPIED) {
+            this.setBackground(Color.LIGHT_GRAY);
+        } else if (status == trackStatus.CLOSED) {
+            this.setBackground(Color.BLACK);
+        } else {
+            this.setBackground(Color.BLUE);
+        }
     }
 }
