ffa799ded3f813401de2e70dc7f6a1c2cc5038ea
thatdaxton <thatdaxton@mt-wireless-pittnet-150-212-27-67.wireless.pitt.edu>
2016-11-29 18:05:44 -0500

    Updated issues with opperations, also made two internal methods private.

--- src/trackModel/UserDBInteraction.java
+++ src/trackModel/UserDBInteraction.java
@@ -2,6 +2,7 @@ package trackModel;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -33,9 +34,16 @@ public class UserDBInteraction {
 	//If there is a match the method will return true, else it will return false
 	public boolean verifyUser(String username, String password) throws SQLException{
 		Statement stmt = conn.createStatement();
-		boolean sucess = stmt.execute("SELECT * FROM TTEDB.Users where UserName = '"+username+"' and UserPW = '"+password+"'");
+		ResultSet rs = stmt.executeQuery("SELECT * FROM TTEDB.Users where UserName = '"+username+"' and UserPW = '"+password+"'");
 		stmt.close();
 
+		String dbUsername = rs.getString(1);
+		String dbPassword = rs.getString(2);
+		boolean sucess = false;
+		
+		if(dbUsername.equals(username) && dbPassword.equals(password))
+			sucess = true;
+		
 		return sucess;
 	}
 }
