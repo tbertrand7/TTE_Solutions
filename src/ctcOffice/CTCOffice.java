9c05a051c782f04b4adf057b5ba4d372eb79c544
Tom Bertrand <tpb18@pitt.edu>
2016-11-30 10:48:39 -0500

    Blocks can now be closed/opened from CTC

--- src/ctcOffice/CTCOffice.java
+++ src/ctcOffice/CTCOffice.java
@@ -1,12 +1,14 @@
 package ctcOffice;
 
 import trackModel.*;
+import trackModel.TrackBlock.*;
 import waysideController.*;
 import java.sql.*;
 
 public class CTCOffice
 {
 	private TrackDBInteraction trackDB;
+	private TrackModel track;
 	
 	public enum Mode {MANUAL, AUTOMATIC}
 	
@@ -19,6 +21,7 @@ public class CTCOffice
 	{
         try {
             trackDB = new TrackDBInteraction();
+			track = new TrackModel();
             greenLine = new TrackBlock[152];
             redLine = new TrackBlock[77];
             loadTrackData();
@@ -63,6 +66,31 @@ public class CTCOffice
 	public Mode getMode()
 	{
 		return mode;
+	}
+
+	public String closeBlock(String line, int block)
+	{
+		TrackBlock currBlock;
+		String rtnStr = "";
+
+		//Get block from appropriate line
+		if (line.equals("Red"))
+			currBlock = redLine[block - 1];
+		else
+			currBlock = greenLine[block-1];
+
+		//If closed set to unoccupied, otherwise set to closed
+		if (currBlock.status == BlockStatus.CLOSED) {
+			currBlock.status = BlockStatus.UNOCCUPIED;
+			rtnStr = line + " Line: Block " + block + " opened";
+		}
+		else {
+			currBlock.status = BlockStatus.CLOSED;
+			rtnStr = line + " Line: Block " + block + " closed for maintenance";
+		}
+
+		track.setBlock(currBlock); //Update block in DB
+		return rtnStr;
 	}
 
 	/**
