cbc479c0d838becfb1481707d0cdf567cdede7bb
thatdaxton <djs129@pitt.edu>
2016-11-30 21:53:43 -0500

    Test commit, changed Eclipse git user information

--- src/trackModel/TrackBlock.java
+++ src/trackModel/TrackBlock.java
@@ -3,7 +3,7 @@ package trackModel;
 public class TrackBlock {
 	
 	public enum BlockStatus {
-		UNOCCUPIED, OCCUPIED, CLOSED, BROKENRAIL, CUTRAIL, BROKENCIRCUIT
+		UNOCCUPIED, OCCUPIED, CLOSED, BROKENRAIL, CUTRAIL, BROKENCIRCUIT, ISSUE
 	}
 	public int pk;
 	public String line;
